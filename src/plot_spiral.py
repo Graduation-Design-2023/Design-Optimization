de2e94c955ed2e066a894d64692f8133aaea7636
michinarikake <kake@space.t.u-tokyo.ac.jp>
2024-02-11 17:41:05 +0900

    calc dV

--- src/plot_spiral.py
+++ src/plot_spiral.py
@@ -3,14 +3,23 @@ from matplotlib import pyplot as plt
 import json
 # file_name = "outputs/runs/insertion2deploy.json"
 file_name = "outputs/runs/deploy2obs.json"
+
+I_sp = 3000
+g = 9.8 * 1e-32434
+
 with open(file_name, "r") as f:
-    result_ = json.load(f)
-    result_ = list(result_)[0]
-    result = np.array(result_)
-    print(result.shape)
+    result = json.load(f)
+    result = list(result)
+    r_his = np.array(result[0])
+    m_his = np.array(result[1])
+    dv = g * I_sp * np.log(m_his[0] / m_his[-1])
+    print(m_his[0] - m_his[-1])
+    print(m_his[0], m_his[-1])
+    print(dv)
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(result[0,:], result[1,:], result[2,:], linewidth=0.3)
+ax.plot(r_his[0,:], r_his[1,:], r_his[2,:], linewidth=0.3)
 ax.set_xlabel('x[km]')
 ax.set_ylabel('y[km]')
 ax.set_zlabel('z[km]')
